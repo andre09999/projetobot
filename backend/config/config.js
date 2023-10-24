d83d69b52a15061fa7341d2cb050f3d4d6b71aa8
Andre <andre@pop-os.localdomain>
2023-10-24 10:26:27 -0300

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
 const config = {
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Secure123',
-  database: process.env.MYSQL_DATABASE || 'BotTelegram',
-  host: process.env.MYSQL_HOST||'127.0.0.1',
+  username: process.env.PGUSER || 'root' || process.env.MYSQL_USER,
+  password: process.env.PGPASSWORD || 'Secure123' || process.env.MYSQL_PASSWORD  ,
+  database: process.env.PGDATABASE || 'BotTelegram' || process.env.MYSQL_DATABASE  ,
+  host: process.env.PGHOST||'127.0.0.1' || process.env.MYSQL_HOST,
   dialect: 'mysql',
-
+  connection: {
+    options: `project=${process.env.ENDPOINT_ID}`,
+  },
 };
+
 
 module.exports = {
   development: config,
