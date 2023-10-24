6c976ade23c6ea96f6e434030250738e913b2606
Andre <andre@pop-os.localdomain>
2023-10-24 12:04:03 -0300

    Create .eslintrc.js

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // ... outras configurações do ESLint ...
+  
+    // Adicione a opção requireConfigFile com o valor false
+    requireConfigFile: false,
+  
+    // ... outras configurações do ESLint ...
+  };
+  
