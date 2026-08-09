744b0723109eb34f09a90eb8a2185cad7fac4099
Andre Luis Ribeiro de Souza <andre09999@live.com>
2026-08-08 21:58:11 -0300

    fix: corrigir script de inicialização do backend

--- backend/init.sh
+++ backend/init.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-npm run dev
-npm install -g sequelize-cli
-npx sequelize db:create
-npx sequelize-cli db:migrate
-npx sequelize-cli db:seed:all
+#!/bin/sh
+
+set -eu
+
+npm run db:migrate
+exec npm start
